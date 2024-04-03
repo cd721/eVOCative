e17a39b3d52a07597c087e2ce4869817b41fa363
cd721 <cdemario@stevens.edu>
2024-04-03 12:16:29 -0400

    add routing

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+import postRoutes from './posts.js';
+import userRoutes from './users.js';
+import path from 'path';
+import { static as staticDir } from 'express';
+const constructorMethod = (app) => {
+    app.use('/posts', postRoutes);
+    app.use('/users', userRoutes);
+    app.get('/about', (req, res) => {
+        res.sendFile(path.resolve('static/about.html'));
+    });
+    app.use('/public', staticDir('public'));
+    app.get('/', (req, res) => {
+        res.render('home');
+    })
+
+    app.use('*', (req, res) => {
+        res.redirect('/');
+    });
+};
+
+export default constructorMethod;
