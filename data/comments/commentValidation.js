78612258e3b601139f6525a5c49437a0261cc53b
Justin Vilchez <Justin Vilchez>
2024-04-03 11:23:28 -0400

    Added validation for posts.

--- data/comments/commentValidation.js
+++ data/comments/commentValidation.js
@@ -1,6 +1,7 @@
 const exportedMethods = {
 
     validateBody(body) {
+        if (!body) throw `Error: Comment not provided.`;
         if (typeof body !== 'string') throw `Error: Comment must be of type string`;
         body = body.trim();
         if (body.length === 0) throw `Error: Comment cannot be empty or just spaces`;
