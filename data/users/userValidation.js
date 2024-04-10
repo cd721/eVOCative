a995e77b0e5fdfae12d8a1cdf184b58f8b391ed4
cd721 <55518903+cd721@users.noreply.github.com>
2024-04-09 20:18:13 -0400

    Update userValidation.js

--- data/users/userValidation.js
+++ data/users/userValidation.js
@@ -1,22 +1,26 @@
 const exportedMethods = {
   validateName(name) {
-    if (!name) throw `Error: Name not provided.`;
-    if (typeof name !== "string")
+    if (!name) { throw `Error: Name not provided.` };
+    if (typeof name !== "string") {
       throw `Error: Name must be of type string`;
-      name = name.trim();
-    if (name.length === 0)
+    }
+    name = name.trim();
+    if (name.length === 0) {
       throw `Error: Name cannot be empty or just spaces`;
+    }
     return name;
   },
 
   validateEmail(email) {
-    if (!email) throw `Error: Email not provided.`;
-    if (typeof email !== "string")
+    if (!email) { throw `Error: Email not provided.` };
+    if (typeof email !== "string") {
       throw `Error: Email must be of type string`;
-      email = email.trim();
-    if (email.length === 0)
+    }
+    email = email.trim();
+    if (email.length === 0) {
       throw `Error: Email cannot be empty or just spaces`;
-    if (email.split("@").length < 2) throw `Error: Email must be a valid email address`;
+    }
+    if (email.split("@").length < 2) { throw `Error: Email must be a valid email address` };
     return email;
   },
 };
