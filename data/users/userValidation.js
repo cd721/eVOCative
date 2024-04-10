2663a442e85767210293a7d0aa1fdb6614060d61
Justin Vilchez <Justin Vilchez>
2024-04-10 12:05:50 -0400

    Added validation for login page, errors for login is WIP

--- data/users/userValidation.js
+++ data/users/userValidation.js
@@ -1,28 +1,22 @@
+import gen from '../../validation/generalValidation.js';
+
 const exportedMethods = {
+
   validateName(name) {
-    if (!name) { throw `Error: Name not provided.` };
-    if (typeof name !== "string") {
-      throw `Error: Name must be of type string`;
-    }
-    name = name.trim();
-    if (name.length === 0) {
-      throw `Error: Name cannot be empty or just spaces`;
-    }
+    name = gen.validateGen('Name', name);
     return name;
   },
 
   validateEmail(email) {
-    if (!email) { throw `Error: Email not provided.` };
-    if (typeof email !== "string") {
-      throw `Error: Email must be of type string`;
-    }
-    email = email.trim();
-    if (email.length === 0) {
-      throw `Error: Email cannot be empty or just spaces`;
-    }
+    email = gen.validateGen('Email', email);
     if (email.split("@").length < 2) { throw `Error: Email must be a valid email address` };
     return email;
   },
+
+  validatePassword(password) {
+    password = gen.validateGen('Password', password);
+    return password;
+  }
 };
 
 export default exportedMethods;
--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,53 @@
 import { Router } from 'express';
-import idValidation from '../validation/idValidation.js';
+import userValidation from '../data/users/userValidation.js';
 import userData from '../data/users/users.js';
-
-import postData from '../data/posts/posts.js'
 const router = Router();
 
 router
-    .route('/').get(async (req, res) => {
-        try {
-            return res.render('login');
-        } catch (e) {
-            return res.status(500).json({error: e});
-        }
-});
+    .route('')
+        .get(async (req, res) => {
+            try {
+                return res.render('login');
+            } catch (e) {
+                return res.status(500).json({error: e});
+            }
+        })
+        .post(async (req, res) => {
+            let login = req.body;
+            let errorList = [];
+            let username = login.loginUser;
+            let password = login.loginPass;
+            let userList;
+
+            try {
+                username = userValidation.validateName(username);
+            } catch (e) {
+                errorList.push(e);
+            }
+
+            try {
+                password = userValidation.validatePassword(password);
+            } catch (e) {
+                errorList.push(e);
+            }
+
+            try {
+                userList = await userData.getAllUsers();
+            } catch (e) {
+                errorList.push(e);
+            }
+            
+            if(errorList.length > 0) {
+                res.render('login', {
+                    errors: errorList,
+                    hasErrors: true,
+                    username: username,
+                    log: login
+                });
+                return;
+            }
+            else {
+                console.log('it work!');
+            }
+        });
 export default router;
