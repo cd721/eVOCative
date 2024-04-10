c4867a9cf0c6974892055e72f2b5b25d4aca8bb8
JRollins2024 <114023339+JRollins2024@users.noreply.github.com>
2024-04-10 18:05:19 -0400

    Register Routes

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import idValidation from "../validation/idValidation.js";
 import userData from "../data/users/users.js";
+import * as valid from "../data/users/userValidation.js";
 
 const router = Router();
+
+const checkPassword = (password, confirmPassword) => {
+  if (password !== confirmPassword) {
+    throw `Error: Passwords do not match`;
+  }
+};
 
 router.route("/").get(async (req, res) => {
   try {
@@ -14,8 +21,11 @@ router.route("/").get(async (req, res) => {
 
 router.route("/").post(async (req, res) => {
   let newUser = req.body;
-  let { fName, lName, email, username, password } = newUser;
+  let { fName, lName, email, username, password, confirmPassword } = newUser;
   try {
+    valid.validateName(fName);
+    valid.validateName(lName);
+    valid.validateEmail(email);
     await userData.addUser(fName, lName, email, username, password);
     return res.render("register");
   } catch (error) {
