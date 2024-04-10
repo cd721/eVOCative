b5d4de0122373ddbd4b30f7a2b9de6f192cb2468
cd721 <cdemario@stevens.edu>
2024-04-10 09:33:05 -0400

    add quiz route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import forumRoutes from './forum.js';
 import userRoutes from './users.js';
+import quizRoutes from './quiz.js'
 import path from 'path';
 import { static as staticDir } from 'express';
 const constructorMethod = (app) => {
     app.use('/forum', forumRoutes);
+    app.use('/quiz', quizRoutes);
     app.use('/posts', forumRoutes);
     app.use('/users', userRoutes);
     app.get('/about', (req, res) => {
