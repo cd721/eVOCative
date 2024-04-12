9b48117127148ac8dd97cd8950f9f094535e4c70
jammskia <jamm222002@gmail.com>
2024-04-12 05:33:39 -0400

    Add more CSS and reformat handlebars

--- data/words/helpers.js
+++ data/words/helpers.js
@@ -2,6 +2,8 @@ import { ObjectId } from "mongodb";
 
 let exportedMethods = {
   createNewWord(word, definition, tags) {
+    let today = new Date();
+
     let newWord = {
       _id: new ObjectId(),
 
@@ -11,6 +13,13 @@ let exportedMethods = {
 
       total_holders: 0,
       accuracy_score: 0,
+
+      dateAdded: today,
+      dateFormatted: {
+        month: today.toLocaleDateString('en-US', { month : 'long'}),
+        day: today.toLocaleDateString('en-US', { day : 'numeric'}),
+        year: today.toLocaleDateString('en-US', { year : 'numeric'})
+      }
     };
 
     return newWord;
