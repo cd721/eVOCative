d6a1246f1d3b3f9187888c26e98060ff37cff463
JRollins2024 <114023339+JRollins2024@users.noreply.github.com>
2024-04-17 10:53:53 -0400

    User validation for registering

--- data/users/userValidation.js
+++ data/users/userValidation.js
@@ -1,30 +1,43 @@
-import gen from '../../validation/generalValidation.js';
+import gen from "../../validation/generalValidation.js";
+import { users } from "../../config/mongoCollections.js";
 
 const exportedMethods = {
-
   validateName(name) {
-    name = gen.validateGen('Name', name);
+    name = gen.validateGen("Name", name);
     return name;
   },
 
   validateEmail(email) {
-    email = gen.validateGen('Email', email);
-    if (email.split("@").length < 2) { throw `Error: Email must be a valid email address` };
+    email = gen.validateGen("Email", email);
+    if (email.split("@").length < 2) {
+      throw `Error: Email must be a valid email address`;
+    }
     return email;
   },
 
-  usernameDoesNotAlreadyExist(username){
+  async usernameDoesNotAlreadyExist(username) {
     //TODO: ensure usernames are unique
+    let userCollection = await users();
+    let duplicateUser = await userCollection.findOne({
+      username: { $regex: new RegExp(`^${username}$`, "i") },
+    });
+    if (duplicateUser)
+      throw `user already exists with the username ${username}`;
+
+    console.log("good username");
+    return username;
   },
 
-  validateUsername(username){
+  validateUsername(username) {
     //TODO: ensure usernames are valid
+    username = gen.validateGen("Username", username);
+    return username;
   },
 
   validatePassword(password) {
-    password = gen.validateGen('Password', password);
+    password = gen.validateGen("Password", password);
     return password;
-  }
+  },
 };
 
 export default exportedMethods;
