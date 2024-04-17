cf52ad9fd9413e82ca02ce67dfd041154bde6ad1
jmcerino <jmcerino13@gmail.com>
2024-04-17 12:12:18 -0400

    A bunch of client-side user registration stuff

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import idValidation from "../validation/idValidation.js";
 import userData from "../data/users/users.js";
-import * as valid from "../data/users/userValidation.js";
+import userValidation from "../data/users/userValidation.js";
 const router = Router();
 
 const checkPassword = (password, confirmPassword) => {
@@ -22,11 +22,47 @@ router.route("/").post(async (req, res) => {
   let newUser = req.body;
 
   try {
+    await userValidation.validateEmail(newUser.email);
+  } catch (e) {
+    return res.render("register", {
+      error: e,
+      fname: newUser.fname,
+      lname: newUser.lname,
+    });
+  }
+
+  try {
+    await userValidation.emailDoesNotAlreadyExist(newUser.email);
+  } catch (e) {
+    return res.render("register", {
+      error: e,
+      fname: newUser.fname,
+      lname: newUser.lname,
+    });
+  }
+
+  try {
+    await userValidation.usernameDoesNotAlreadyExist(newUser.username);
+  } catch (e) {
+    return res.render("register", {
+      error: e,
+      fname: newUser.fname,
+      lname: newUser.lname,
+      email: newUser.email,
+      username: newUser.username,
+    });
+  }
+
+  try {
     checkPassword(newUser.password, newUser.confirmPassword);
   } catch (e) {
-    return res.render("register", {error: e});
+    return res.render("register", {
+      error: e,
+      fname: newUser.fname,
+      lname: newUser.lname,
+      email: newUser.email,
+    });
   }
-  
 
   let userAdded;
 
@@ -36,9 +72,9 @@ router.route("/").post(async (req, res) => {
       newUser.lname,
       newUser.email,
       newUser.username,
-      newUser.password,
+      newUser.password
     );
-    return res.render("users/profile");
+    return res.redirect(`users/${userAdded._id}`);
   } catch (e) {
     console.log(e);
     return res.status(400).render("error", { error: e });
